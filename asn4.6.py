cd08ff28a43fa0c329ab3f280d68bd3ecc7c54a1
ayushmohanty24 <63730038+ayushmohanty24@users.noreply.github.com>
2021-06-25 12:24:26 +0530

    Create asn4.6.py

---
+++ asn4.6.py
@@ -0,0 +1,18 @@
+Write a program to prompt the user for hours and rate per hour using input to compute gross pay.
+Pay should be the normal rate for hours up to 40 and time-and-a-half for the hourly rate for all hours worked above 40 hours.
+
+
+
+hours=input('hours:')
+rate=input('rate:')
+h=float(hours)
+r=float(rate)
+
+def computepay(h, r):
+    if h<40 :
+        return h*r
+    overtime = h - 40.0               
+    return (r * 40.0) + (1.5 * r * overtime) 
+
+p = computepay(h,r)
+print("Pay", p)
